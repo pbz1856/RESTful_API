70c1431a6fe5e25c84ef0710e85a5c829652b07d
Todd <pbz1856@gmail.com>
2020-05-06 20:58:15 +0800

    Adding testing endpoints

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening port 3000...")
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening port ${port}...`);
 });
 
+app.get('/', (req, res, next) => {
+  res.send('Welcome to the API endpont');
+});
+
+app.get('/api/genres', (req, res, next) => {
+
+  res.send('This is Genres');
+});
+
+// TODO
+
+// 1. Think about the data set I will be using for my API endpoint
+// 2. Validator
+// 3. Error handling - middleware, winston, etc.
+// 4. Connecting to MongoDB
+//
