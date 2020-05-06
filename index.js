77a00662cc05c9fedd5b61760c79d5aa946e3957
pbz1856 <ubuntu@ip-172-31-29-168.ap-southeast-1.compute.internal>
2020-05-06 06:58:50 +0000

    Updated port # back to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Listening port 5000...")
+app.listen(3000, () => {
+  console.log("Listening port 3000...")
 });
 
